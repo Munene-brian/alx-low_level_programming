0b96f06bf56383584e09e2c327f91440a4996109
Munene-brain <brianmunene001@gmail.com>
2022-06-30 13:43:42 -0700

    updated type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,20 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: 0 if successful
+ * Return: 0 if success
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
 return (0);
 }
