1f13b2e02977b56419bf9e3337c83417739e595a
Munene-brain <brianmunene001@gmail.com>
2022-07-05 13:47:29 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower-> check if is lower
+ * @c: a character
+ *
+ * Return: return 1 and 0 depending
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
