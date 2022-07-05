e9e0099cb66856133bc36aac2172e73cbbdc6b2b
Munene-brain <brianmunene001@gmail.com>
2022-07-05 14:03:30 -0700

    fixed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,12 @@
 
 /**
  * _isalpha -checks for alphabets
- * @c: a character
+ * @c: a character to check
+ *
+ * Return: 0 or 1 depending
  *
  */
 int _isalpha(int c)
 {
-return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
