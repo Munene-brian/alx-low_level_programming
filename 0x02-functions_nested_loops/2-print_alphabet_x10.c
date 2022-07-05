17e68730143f7245ce5b4c9904a3fffc4b8b7012
Munene-brain <brianmunene001@gmail.com>
2022-07-05 13:36:27 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+int a;
+int b;
+for (b = 0; b < 10; b++)
+{
+for (a  = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
