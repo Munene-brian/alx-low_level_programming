cf84af67ec6f3bf7f48eaf538a76cea1f14878a5
Munene-brain <brianmunene001@gmail.com>
2022-07-05 17:03:10 -0700

    add two numers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -add two numbers
+ * @a: first par
+ * @b: sec par
+ *
+ * Description: add two numbers
+ * Return: always (0)
+ */
+int add(int a, int b)
+{
+ return (a + b);
+}
