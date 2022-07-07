bfe72902414739aed13554858febf060f33f250c
Munene-brain <brianmunene001@gmail.com>
2022-07-06 23:41:57 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checkes uppercase
+ * @c: the chacter
+ * Return: 1
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme forloops functions
