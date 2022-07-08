0f07da83f44f14b4d8df6b273aedce58d7ab7302
Munene-brain <brianmunene001@gmail.com>
2022-07-08 01:15:16 -0700

    3 number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_number - print the number 
+ * print_numbers - print the number
  *
+ * Return: return success
  */
 void print_numbers(void)
 {
 int i;
 
-for(i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
 }
