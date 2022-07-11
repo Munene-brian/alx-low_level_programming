610fe0b4ef73775e8b63344ed7faac4bd336e424
Munene-brain <brianmunene001@gmail.com>
2022-07-11 09:57:14 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,21 +2,23 @@
 
 /**
  * puts2 - prints one char out of 2
- * @str: type
+ * @str: input type
+ *
+ * Return: no return.
  */
 void puts2(char *str)
 {
-int i, j = 0;
+int i = 0;
 
-while (str[i] != '\0')
+while (i >= 0)
 {
+if (str[i] == '\0')
+{
+_putchar('\n');
+break;
+}
+if (i % 2 == 0)
+	_putchar(str[i]);
 i++;
 }
-
-while (j < i)
-{
-_putchar(str[j]);
-j = j + 2;
-}
-_putchar('\n');
 }
