aee191b3277e63a6ed8df274b6e30a6538fdb74e
Munene-brain <brianmunene001@gmail.com>
2022-07-11 10:18:00 -0700

    task 8 updated

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,2 +1,20 @@
 #include "main.h"
 
+/**
+ * print_array - prints elements
+ * @a: input array
+ * @n: size of elements
+ * Return: no return
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+
+for (; i < n; i++)
+{
+printf("%d", *(a + i));
+if (i != (n - 1))
+	printf(", ");
+}
+printf("\n");
+}
