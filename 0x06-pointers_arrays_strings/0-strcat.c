35547e31ddbda0aea2d1c6f4ffe21dff578657d4
Munene-brain <brianmunene001@gmail.com>
2022-07-13 13:05:25 -0700

    hot fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,21 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-int j = 0;
+int len = 0, i;
 
-while (dest[i] != '\0')
+while (dest[len])
+	len++;
+
+for (i = 0; src[i] != '\0'; i++)
 {
-i++;
+dest[len] = src[i];
+len += 1;
 }
-
-while (src[i] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-
-dest[i] = '\0';
+dest[len] = '\0';
 return (dest);
 }
