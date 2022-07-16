a5501847bf8e1f4cc00b2da7cff7759547715dd9
Munene-brain <brianmunene001@gmail.com>
2022-07-16 03:06:57 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * *_strcat -functions to commute
  * @dest: parameter
@@ -9,24 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+int index = 0, dest_len = 0;
 
-i = 0;
+while (dest[index++])
+	dest_len++;
 
-while (dest[i] != '\0')
-{
-i++;
-}
-
-j = 0;
-
-while (src[i] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
+for (index = 0; src[index]; index++)
+	dest[dest_len++] = src[index];
 
 return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
+#define MAIN_H
+#include <stdio.h>
 char *_strcat(char *dest, char *src);
 char *strncat(char *dest, char *src, int n);
 char *strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void revers_array(int *a, int n);
+
+#endif /*MAIN_H*/
