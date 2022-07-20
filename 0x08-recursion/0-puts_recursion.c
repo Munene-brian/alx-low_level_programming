577fffd6a61c44fb5f403aae92aa8c9dcf6f0f4e
Munene-brain <brianmunene001@gmail.com>
2022-07-20 02:19:21 -0700

    task /0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string
+ * @s: string
+ * Return: no return
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar (*s);
+_put_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
